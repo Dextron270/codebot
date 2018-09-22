6de65782342177f5cba7ff8d41883a8e2bc5edb0
ZDXT <destroyer2750@gmail.com>
2018-09-21 23:04:40 -0300

    now its working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import discord
 import sys
-import asyncio
+#import asyncio
 import subprocess
 
 client = discord.Client()
@@ -17,14 +17,14 @@ async def run_script(file, channel):
     msg = await channel.send(":)")
     
     for line in stdout:
-        msg_content = ((msg.content)[9:])[:-3]
+        msg_content = ((msg.content)[10:])[:-3]
 
         if len(msg_content) < 15:
             msg_content_tosend = msg_content
 
         else:
-            print("a")
-            msg_content_tosend = msg_content.split("\n", maxsplit=1)[1]
+            msg_content_tosend = (msg_content.partition("\n"))[2]
+            print(msg_content.partition("\n"))
 
         to_send = f"```python\n{msg_content_tosend}{line.decode('ascii')}```"
         await msg.edit(content=to_send)
